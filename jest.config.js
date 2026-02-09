44af6038a4d66f16a964a288f874532b9a1fbe0e
0xkkkn <snigerev.vladimir.e@gmail.com>
2026-02-09 18:19:49 +0300

    ci: add Codecov patch coverage enforcement (#7)
    Remove global 80% coverage threshold from jest config and replace
    artifact upload with Codecov integration for 90% patch coverage on PRs.

--- jest.config.js
+++ jest.config.js
@@ -15,14 +15,6 @@ module.exports = {
     '!src/infrastructure/logging/**',
   ],
   coverageDirectory: './coverage',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
